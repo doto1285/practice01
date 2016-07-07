46ddbfe69ea2d64f7f5f1a99c12c525c2a31e949
donghyo <eotkdehwnd12@naver.com>
2016-07-07 10:07:14 +0900

    practice01

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -1,8 +1,25 @@
 package practice01;
 
 public class Game369 {
-
 	public static void main(String[] args) {
-
+		for (int i = 1; i < 100; i++) {
+			String sNum = String.valueOf(i);
+			char[] temp = sNum.toCharArray();
+			boolean isclap = false;
+			for (int j : temp) {
+				if (j == '3' || j == '6' || j == '9') {
+					if (!isclap) {
+						isclap = true;
+						System.out.print(i + " 짝");
+					} else {
+						System.out.print("짝");
+					}
+				}
+			}
+			if(isclap) {
+				System.out.println();
+			}
+			isclap = false;
+		}
 	}
 }
